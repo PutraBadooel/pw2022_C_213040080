e560fcf2e4c90c29e7d6b46392f30fa7643d9814
PutraBadooel <putrabadul8@gmail.com>
2022-03-02 12:33:48 +0800

    latihan pertemuan 3

---
+++ kuliah/pertemuan3/latihan1.php
@@ -0,0 +1,28 @@
+<?php 
+// pengulangan
+//for
+//while
+//do.. while
+// foreach : pengulangan khusus array
+
+//for
+for( $i = 0; $i < 5; $i++ ) {
+    echo "hello world! <br>";
+}
+
+//while
+$i = 0;
+while( $i < 5) {
+    echo "hello world! <br>";
+    $i++;
+}
+
+//do while
+$i = 0;
+do {
+echo "hello world! <br>";
+$i++;
+}while( $i < 5 );
+
+
+?>
