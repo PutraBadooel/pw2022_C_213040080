d67700a6fed493d96344d04dd1ba08e744112fc5
PutraBadooel <putrabadul8@gmail.com>
2022-03-02 10:58:53 +0800

    latihan pertemuan 2

---
+++ kuliah/pertemuan2/index.php
@@ -0,0 +1,69 @@
+<?php 
+// pertemuan - php dasar
+// sintaks PHP
+
+// standar output
+// echo, print
+// print_r
+// var_dump (digunakan untuk debugging)
+
+// echo "Putra Rizqi Pratama";
+
+
+// penulisan sintaks
+// 1. php dalam html
+/// 2. html dalam php (tidak disarankan)
+
+// variabel dan tipe data
+// variabel
+// tidak boleh di awali dengan angka, tapi boleh mengandung angka
+
+// $nama = "Putra Rizqi";
+// echo "halo, nama saya $nama";
+
+// operator
+//aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+
+// penggabung string / concatenation/ concat
+// .
+// $nama_depan = "Putra";
+// $nama_belakang = "Rizqi Pratama";
+// echo $nama_depan . " " . $nama_belakang
+
+// assignment (operator penugasan)
+// =, += , *=, /=, %=, .=
+// $x = 1;
+// $x -= 5;
+// echo $x;
+// $nama = "Putra";
+// $nama .= " ";
+// $nama .= "Rizqi Pratama";
+// echo $nama;
+
+// perbandingan
+// <,>< <=, >=, ==, !=
+// var_dump (1 < 5);
+// var_dump (1 > 5);
+// var_dump (1 = 5);
+// var_dump(1 == "1");
+
+//identitas
+// ===, !==
+// var_dump(1 === "1");
+
+//logika
+// &&, ||, !
+$x = 10;
+// var_dump($x < 20 && $x % 2 == 0);
+var_dump($x < 20 || $x % 2 == 0);
+
+
+
+
+?>
+
